d8090074c73772e0dc1118f20558854832593a7d
Kira <kawoods826@gmail.com>
2019-09-22 14:12:58 -0700

    Imported useState hook, established applications state for home and away score, and added state values to scoreboard.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  
+  //You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(0); //Lions Score
+  const [awayScore, setAwayScore] = useState(0); //Tigers Score
 
+  
   return (
     <div className="container">
       <section className="scoreboard">
@@ -13,14 +17,15 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+            {/* TODO STEP 3 - We need to change the hardcoded values in these 
+            divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
